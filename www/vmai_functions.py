0a52acb74a3f6577d223a3ff02ae35364910bdd0
Michal Slowik <mislowik@cisco.com>
2020-10-20 13:05:48 +0200

    Support for local json DB

--- www/vmai_functions.py
+++ www/vmai_functions.py
@@ -1,7 +1,8 @@
 # VMware Auto-Installer functions
+#from config_local import *
 from config import *
-from os import system
-import re
+from os import system, path
+import re, json
 
 def generate_kickstart(rootpw, hostname, ipaddr, subnet, netmask, gateway, vmnicid, kscfg, enablessh=False, clearpart=False):
     # uses KSTEMPLATE to build custom kickstart config based on provided parameters
@@ -25,6 +26,8 @@ def generate_kickstart(rootpw, hostname, ipaddr, subnet, netmask, gateway, vmnic
         # check if "Enable SSH" has been set
         if enablessh:
             ksfile.write(SSHTXT)
+        else:
+            ksfile.write(NOSSHTXT)
     ksfile.close()
     kstemplate_file.close()
 
@@ -50,6 +53,7 @@ def generate_pxe(ksurl, isofile, macaddr):
         pxefile.write(pxetemplate)
     pxefile.close()
     pxetemplate_file.close()
+    return ISOVER
 
 def generate_dhcp(hostname, subnet, netmask, ipaddr, gateway, macaddr):
     dhcptemplate_file = open(DHCPTEMPLATE)
@@ -82,3 +86,97 @@ def generate_efi(ksurl, macaddr):
     bootcfg = re.sub(r"kernelopt.*", kerneloptline, bootcfg)
     with open(bootcfg_custom, 'w+') as bootcfg_custom_file:
         bootcfg_custom_file.write(bootcfg)
+
+def save_install_data_to_db(hostname, mac, ipaddr, subnet, netmask, gateway, vlan, vmnic,
+                            enablessh, clearpart, rootpw, isover, status):
+    if path.isfile(VMAI_DB):
+        print('[INFO] ' + VMAI_DB + ' file exists - importing data and adding new entry:')
+        with open(VMAI_DB, 'r') as vmaidb_file:
+            vmaidb_dict = json.load(vmaidb_file)
+            # need to add checking of 'hostname' already exists ion VMAI_DB
+            vmaidb_dict[hostname] = {}
+            vmaidb_dict[hostname]['MAC'] = mac
+            vmaidb_dict[hostname]['IPADDR'] = ipaddr
+            vmaidb_dict[hostname]['SUBNET'] = subnet
+            vmaidb_dict[hostname]['NETMASK'] = netmask
+            vmaidb_dict[hostname]['GATEWAY'] = gateway
+            vmaidb_dict[hostname]['VLAN'] = vlan
+            vmaidb_dict[hostname]['VMNIC'] = vmnic
+            vmaidb_dict[hostname]['SSH'] = enablessh
+            vmaidb_dict[hostname]['CLEARPART'] = clearpart
+            vmaidb_dict[hostname]['ROOTPW'] = rootpw
+            vmaidb_dict[hostname]['ISO'] = isover
+            vmaidb_dict[hostname]['STATUS'] = status
+            print(vmaidb_dict)
+            vmaidb_file.close()
+        with open(VMAI_DB, 'w+') as vmaidb_file:
+            json.dump(vmaidb_dict, vmaidb_file, ensure_ascii=False, indent=2)
+            vmaidb_file.close()
+    else:
+        print('[INFO] ' + VMAI_DB + ' file DOES NOT exist - creating file and adding new entry')
+        vmaidb_dict = {}
+        vmaidb_dict[hostname] = {}
+        vmaidb_dict[hostname]['MAC'] = mac
+        vmaidb_dict[hostname]['IPADDR'] = ipaddr
+        vmaidb_dict[hostname]['SUBNET'] = subnet
+        vmaidb_dict[hostname]['NETMASK'] = netmask
+        vmaidb_dict[hostname]['GATEWAY'] = gateway
+        vmaidb_dict[hostname]['VLAN'] = vlan
+        vmaidb_dict[hostname]['VMNIC'] = vmnic
+        vmaidb_dict[hostname]['SSH'] = enablessh
+        vmaidb_dict[hostname]['CLEARPART'] = clearpart
+        vmaidb_dict[hostname]['ROOTPW'] = rootpw
+        vmaidb_dict[hostname]['ISO'] = isover
+        vmaidb_dict[hostname]['STATUS'] = status
+        print(vmaidb_dict)
+        with open(VMAI_DB, 'w+') as vmaidb_file:
+            json.dump(vmaidb_dict, vmaidb_file, ensure_ascii=False, indent=2)
+            vmaidb_file.close()
+
+def print_vmai_db():
+    # print VMAI_DB summary to stdout for debugging purposes
+    with open(VMAI_DB, 'r') as vmaidb_file:
+        vmaidb = json.load(vmaidb_file)
+        print("{:25} {:20} {:20} {:30}"
+              .format('Hostname', 'MAC address', 'IP Address', 'STATUS'))
+        for host in vmaidb.items():
+            print("{:25} {:20} {:20} {:30}"
+                  .format(host[0], host[1]['MAC'], host[1]['IPADDR'], host[1]['STATUS']))
+    print('')
+    return vmaidb
+
+def deployment_status(ipaddr, mac):
+    # Stage 0: 'Ready to deploy' - initial state after submitting 'Launch automation'
+
+    # print('[INFO] Checking for Stage 1...')
+    # # Stage 1: 'DHCP request'    DHCPDISCOVER from 3c:57:31:ea:89:d8 via ens224
+    # for line in tail("-10f", "../TMP/messages", _iter=True):
+    #     if all(x in line for x in ['DHCPDISCOVER', mac]):
+    #         print('[INFO] Found correct line:')
+    #         print(line)
+    #         deployment_status = 'DHCP request'
+    #         break
+    #     else:
+    #         print(line)
+    #         print('[INFO] Waiting for DHCPDISCOVER...')
+    #
+    # print('[INFO] Checking for Stage 2...')
+    # # Stage 2: 'Bootloader request'        Client ::ffff:192.168.100.2 finished /01-3c-57-31-ea-89-d8/boot.cfg
+    # for line in tail("-10f", "../TMP/messages", _iter=True):
+    #     if all(x in line for x in ['boot.cfg', mac.replace(':', '-')]):
+    #         print('[INFO] Found correct line:')
+    #         print(line)
+    #         deployment_status = 'Bootloader request'
+    #         break
+    #     else:
+    #         print(line)
+    #         print('[INFO] Waiting for Bootloader request...')
+
+    # print('[INFO] Checking for Stage 3...')
+    system('wget --timeout=3 -t 1 --no-check-certificate https://' + ipaddr + '/READY -O /tmp/READY-' + ipaddr + '>/dev/null 2>&1')
+    if path.isfile('/tmp/READY-' + ipaddr) and path.getsize('/tmp/READY-' + ipaddr) > 0:
+        deployment_status = 'Finished'
+    else:
+        deployment_status = 'Installation in progress'
+
+    return deployment_status
