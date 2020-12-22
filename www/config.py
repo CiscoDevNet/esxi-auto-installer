b74552e1cd7374315652570f375a2f8421e6e1c7
Michal Slowik <mislowik@cisco.com>
2020-12-22 16:20:11 +0100

    config adapted to use kickstart.jinja template

--- www/config.py
+++ www/config.py
@@ -9,24 +9,6 @@ TMPISODIR = DOCROOT + 'upload/mnt/'
 TFTPISODIR = TFTPBOOT + 'iso/'
 KSDIR = TFTPBOOT + 'ks/'
 PXEDIR = TFTPBOOT + 'pxelinux.cfg/'
-KSTEMPLATE = DOCROOT + 'templates/kickstart_template'
+KSTEMPLATE = DOCROOT + 'templates/kickstart.jinja'
 PXETEMPLATE = DOCROOT + 'templates/pxecfg_template'
 DHCPTEMPLATE = DOCROOT + 'templates/dhcp_template'
-SSHTXT = """%firstboot --interpreter=busybox
-# enable & start remote ESXi Shell  (SSH)
-vim-cmd hostsvc/enable_ssh
-vim-cmd hostsvc/start_ssh
-# disable IPv6
-esxcli network ip set --ipv6-enabled=false
-echo "`date` - Server `hostname` installation finished." >/usr/lib/vmware/hostd/docroot/READY
-sleep 75
-reboot
-"""
-NOSSHTXT = """%firstboot --interpreter=busybox
-# disable IPv6
-esxcli network ip set --ipv6-enabled=false
-echo "`date` - Server `hostname` installation finished." >/usr/lib/vmware/hostd/docroot/READY
-sleep 75
-reboot
-"""
-
