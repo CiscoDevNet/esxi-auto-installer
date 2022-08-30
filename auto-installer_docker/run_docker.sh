0c770052fc3103cd95fa4645b3d4451af70c9ff2
Michal Slowik <mislowik@cisco.com>
2022-08-30 16:51:22 +0200

    Pass host IP, gateway, subnet and netmask to docker containers as system variables

--- auto-installer_docker/run_docker.sh
+++ auto-installer_docker/run_docker.sh
@@ -1,9 +1,24 @@
-echo killing old docker processes
+echo Killing old docker processes
 docker-compose rm -fs
 
-echo export host IP address
-export EAI_HOST_IP=`python3 /opt/eai/get_host_ip.py`
-echo $EAI_HOST_IP
+# echo export host IP address
+# export EAI_HOST_IP=`python3 /opt/eai/get_host_ip.py`
+# echo $EAI_HOST_IP
 
-echo building docker containers
+echo Exporting host network settings
+
+NETWORK_DATA=( `python3 get_host_network_settings.py` )
+export EAI_HOST_IP=${NETWORK_DATA[0]}
+export EAI_HOST_GW=${NETWORK_DATA[1]}
+export EAI_HOST_SUBNET=${NETWORK_DATA[2]}
+export EAI_HOST_NETMASK=${NETWORK_DATA[3]}
+
+echo
+echo EAI_HOST_IP: $EAI_HOST_IP
+echo EAI_HOST_GW: $EAI_HOST_GW
+echo EAI_HOST_SUBNET: $EAI_HOST_SUBNET
+echo EAI_HOST_NETMASK: $EAI_HOST_NETMASK
+echo
+
+echo Building docker containers
 docker-compose up --build -d
